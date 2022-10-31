79564f7d5a29ae35fe7ae638313975202b90153b
eval Nya <60149113+nexplorer-3e@users.noreply.github.com>
2022-10-31 11:18:53 +0800

    update start script

--- trsr/eulaceura-test-1026/startvm.cmd
+++ trsr/eulaceura-test-1026/startvm.cmd
@@ -2,7 +2,7 @@ call qemu-system-riscv64 ^
 -smp 4 -m 4G ^
 -machine virt -bios fw_boot.bin ^
 -spice disable-ticketing=on,port=16969 ^
-
+-display sdl,gl=on ^
 -device virtio-gpu ^
 -device virtio-blk-device,drive=hd0 -drive file=sys_disk.qcow2,id=hd0,format=qcow2 ^
 -device virtio-net-device,netdev=usernet -netdev user,id=usernet,ipv6=off,hostfwd=tcp::2222-:22,hostfwd=tcp::5900-:5900,hostfwd=udp::177-:177 ^
@@ -10,7 +10,6 @@ call qemu-system-riscv64 ^
 -device usb-audio,audiodev=snd0 -audiodev sdl,id=snd0 ^
 -device usb-kbd -device usb-tablet 
 
-REM -display gtk,show-tabs=on ^
 
 REM eulaceura does not have modules for usb-audio
 
